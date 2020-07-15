c3e91953f843a8209d250fb9210ab9312a7210de
arianahabas <arianahabas@gmail.com>
2020-07-15 18:41:00 -0500

    MVP

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -3,19 +3,35 @@ import axios from 'axios';
 
 
 export default function Picture (){
-    const [photos, setPhotos]= useState('')
+    const [title, setTitle] = useState('')
+    const [image, setImage] = useState('')
+    const [date, setDate] = useState('')
+    const [explanation, setExplanation] = useState('')
+
     useEffect(()=>{
         axios
         .get('https://api.nasa.gov/planetary/apod?api_key=RF6gFc8i2pt3uCpFUH2gbmbA8a09X9Duad6lhnps')
         .then(res => {
             console.log('The res was', res)
-            setPhotos(res.data)
+            setTitle(res.data.title)
+            setImage(res.data.url)
+            setDate(res.data.date)
+            setExplanation(res.data.explanation)
+            
         })
         .catch(err =>{
             console.log('The error was', err)
         })
     },[])
-    return null
+    
+    return (
+        <div>
+            <div>{title}</div>
+            <img src={image} alt= '' ></img>
+            <div>{date}</div>
+            <div>{explanation}</div>
+        </div>
+    )
 }
 
     
