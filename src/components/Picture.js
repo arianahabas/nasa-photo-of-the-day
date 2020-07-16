976d708575fc28a805ec115989eb7c0aed1c0dde
arianahabas <arianahabas@gmail.com>
2020-07-16 13:29:59 -0500

    updates styles

--- src/components/Picture.js
+++ src/components/Picture.js
@@ -1,35 +1,57 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios';
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
+
+
+const kf = keyframes`
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(0.3);
+    opacity: 0.5;
+  }
+  100% {
+    transform: scale(1) rotate(360deg);
+    opacity: 1;
+  }
+`
 
 const Container = styled.div`
    padding: 0 75px;
    margin: 20px;
    border: 3px solid #282c34;
+   transform: scale(0);
+   animation: ${kf} 1s ease-in-out forwards;
 
-`;
-
-const Title = styled.h2`
-    border: 3px solid #D96C06;
+h2{
+    border: 5px solid #D96C06;
     color: white;
     background-color: #E01A4F;
     border-radius: 10px;
-    align:center;
-`;
+    &:hover{
+        background-color:white;
+        color:#E01A4F;
+    }
+}
 
-const Info = styled.p`
-    padding: 0 40px;
-`;
 
-const Date = styled.div`
-    color: #53B3CB;
+p {
     font-size: 1.3rem;
-    padding: 10px 0;
-
-`;
-const Image = styled.img`
+    padding: 10px 40px;
+    &:nth-of-type(2) {
+      color: #E01A4F;
+    }
+}
+    
+img{
     width: auto;
-`;
+    &:hover {
+      border: 10px solid white;
+    }
+}
+`   
+
 
 export default function Picture (){
     const [title, setTitle] = useState('')
@@ -56,10 +78,10 @@ export default function Picture (){
     return (
         <Container className = "container">
             <div>
-                <Title>{title}</Title>
-                <Info>{explanation}</Info> 
-                <Date>Date: {date}</Date>
-                <Image src={image} alt= '' ></Image>
+                <h2>{title}</h2>
+                <p>{explanation}</p> 
+                <p>Date: {date}</p>
+                <img src={image} alt= '' ></img>
             </div>
             
         </Container>
